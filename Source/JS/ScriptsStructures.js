2fc6f66dcadcc079855641885c08c5d2220429f1
TaDmitry <tarasenkodmitriy000@gmail.com>
2024-02-06 14:21:32 +0600

    beta-0.6.1

--- Source/JS/ScriptsStructures.js
+++ Source/JS/ScriptsStructures.js
@@ -8,3 +8,21 @@ document.getElementById("structure__nav-ul-departments").addEventListener("click
 	document.getElementById("structure__content-departments").style.display = "block";
 	document.getElementById("structure__content-guide").style.display = "none";
 });
+
+window.onload = function () {
+	function replacePhoneLink() {
+		// Получаем ссылку и размер экрана
+		var phoneLink = document.querySelector("#phoneLink");
+		var screenWidth = window.innerWidth;
+
+		if (screenWidth <= 768) {
+			phoneLink.href = "tel:+77777777777";
+		} else {
+			phoneLink.href = "";
+		}
+	}
+
+	// Вызываем функцию при загрузке страницы и изменении размера экрана
+	replacePhoneLink();
+	window.addEventListener("resize", replacePhoneLink);
+};
