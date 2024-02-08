a96002c6439c0f1d8def2eb06edd33e1eab0da8e
TaDmitry <tarasenkodmitriy000@gmail.com>
2024-02-09 00:04:15 +0600

    beta-0.6.93

--- Source/JS/Parallax.js
+++ Source/JS/Parallax.js
@@ -1,64 +1,67 @@
-"use strict";
+// "use strict";
 
-// Выбираем необходимые DOM-элементы
-const parallax = document.querySelector(".parallax");
-const background = document.querySelector(".images-parallax__background");
-const buttons = document.querySelectorAll(".structure__nav-ul-li");
+// // Выбираем необходимые DOM-элементы
+// const parallax = document.querySelector(".parallax");
+// const background = document.querySelector(".images-parallax__background");
+// const buttons = document.querySelectorAll(".structure__nav-ul-li");
 
-// Коэффициенты для параллакса
-const forBackground = 40;
-const speed = 0.05;
+// // Коэффициенты для параллакса
+// const forBackground = 40;
+// const speed = 0.05;
 
-// Переменные для отслеживания координат мыши
-let positionX = 0,
-	positionY = 0;
-let coordXprocent = 0,
-	coordYprocent = 0;
+// // Переменные для отслеживания координат мыши
+// let positionX = 0,
+// 	positionY = 0;
+// let coordXprocent = 0,
+// 	coordYprocent = 0;
 
-// Функция для установки стилей параллакса при движении мыши
-function setMouseParallaxStyle() {
-	const distX = coordXprocent - positionX;
-	const distY = coordYprocent - positionY;
+// // Функция для установки стилей параллакса при движении мыши
+// function setMouseParallaxStyle() {
+// 	const distX = coordXprocent - positionX;
+// 	const distY = coordYprocent - positionY;
 
-	positionX = positionX + distX * speed;
-	positionY = positionY + distY * speed;
+// 	positionX = positionX + distX * speed;
+// 	positionY = positionY + distY * speed;
 
-	// Применяем стили к заднему фону
-	background.style.cssText = `transform: translate(${positionX / forBackground}%,${
-		positionY / forBackground
-	}%);`;
+// 	// Применяем стили к заднему фону
+// 	background.style.cssText = `transform: translate(${positionX / forBackground}%,${
+// 		positionY / forBackground
+// 	}%);`;
 
-	// Запускаем анимацию следующего кадра
-	requestAnimationFrame(setMouseParallaxStyle);
-}
+// 	// Запускаем анимацию следующего кадра
+// 	requestAnimationFrame(setMouseParallaxStyle);
+// }
 
-// Инициализация параллакса при движении мыши
-setMouseParallaxStyle();
+// // Проверяем ширину экрана перед инициализацией параллакса и обработчика события мыши
+// if (window.innerWidth > 768) {
+// 	// Инициализация параллакса при движении мыши
+// 	setMouseParallaxStyle();
 
-// Обработчик движения мыши для определения координат
-parallax.addEventListener("mousemove", function (e) {
-	const parallaxWidth = parallax.offsetWidth;
-	const parallaxHeight = parallax.offsetHeight;
+// 	// Обработчик движения мыши для определения координат
+// 	parallax.addEventListener("mousemove", function (e) {
+// 		const parallaxWidth = parallax.offsetWidth;
+// 		const parallaxHeight = parallax.offsetHeight;
 
-	const coordX = e.pageX - parallaxWidth / 2;
-	const coordY = e.pageY - parallaxHeight / 2;
+// 		const coordX = e.pageX - parallaxWidth / 2;
+// 		const coordY = e.pageY - parallaxHeight / 2;
 
-	// Получаем процентные координаты
-	coordXprocent = (coordX / parallaxWidth) * 100;
-	coordYprocent = (coordY / parallaxHeight) * 100;
-});
+// 		// Получаем процентные координаты
+// 		coordXprocent = (coordX / parallaxWidth) * 100;
+// 		coordYprocent = (coordY / parallaxHeight) * 100;
+// 	});
+// }
 
-// Визуальное отображение активного блока навигации
-buttons.forEach((button) => {
-	button.addEventListener("click", () => {
-		// Снимаем выделение с других кнопок
-		buttons.forEach((btn) => btn.classList.remove("structure__nav-ul-li-activated"));
-		// Выделяем текущую кнопку
-		button.classList.add("structure__nav-ul-li-activated");
-	});
-});
+// // Визуальное отображение активного блока навигации
+// buttons.forEach((button) => {
+// 	button.addEventListener("click", () => {
+// 		// Снимаем выделение с других кнопок
+// 		buttons.forEach((btn) => btn.classList.remove("structure__nav-ul-li-activated"));
+// 		// Выделяем текущую кнопку
+// 		button.classList.add("structure__nav-ul-li-activated");
+// 	});
+// });
 
-// Сокрашения ЦРИ
+// Сокращения ЦРИ
 function replaceTextInH3() {
 	var parallax__h3 = document.getElementById("parallax__h3");
 
