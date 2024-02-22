837a4abca79961cc3331bc84f8ed1ac44713193d
TaDmitry <tarasenkodmitriy000@gmail.com>
2024-02-22 19:06:21 +0600

    beta-0.8

--- Source/JS/Scripts.js
+++
@@ -1,7 +0,0 @@
-// Получаем текущий год
-const currentYear = new Date().getFullYear();
-const elements = document.getElementsByClassName("current-year");
-
-for (let i = 0; i < elements.length; i++) {
-	elements[i].textContent = currentYear;
-}
---
+++ src/assets/js/main.js
@@ -0,0 +1,11 @@
+import './style.js'
+
+import './header/parallaxScripts.js'
+
+import './nav/burgervScripts.js'
+
+import './nav/languageСhangeScripts.js'
+
+import './nav/scrollScripts.js'
+
+import './structure/structuresScripts.js'
